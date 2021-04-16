0e0c129746782bb6a08059425d30146c82dda953
cairo387 <mifuneri@outlook.jp>
2021-04-16 09:46:56 +0000

    [Add]ユーザー編集画面

--- app/controllers/user/users_controller.rb
+++ app/controllers/user/users_controller.rb
@@ -4,8 +4,22 @@ class User::UsersController < ApplicationController
   end
   
   def edit
+    @user = User.find(params[:id])
   end
   
   def update
+    @user = User.find(params[:id ])
+    if @user.update(user_params)
+      redirect_to user_user_path(@user.id)
+    else
+      @user = User.find(params[:id ])
+      render :edit
+    end
+  end
+  
+  private
+  def user_params
+    params.require(:user).permit( :nickname, :name, :postal_code,:prefecture_code, :address_city, :address_street,
+    :phone_number, :email, :image)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+   #画像投稿用(refile)
+  attachment :image
 end
